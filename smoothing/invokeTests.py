1887604aa014579d589736b90032bd02b21996d7
Mateusz Kordowski <mateuszkd67@gmail.com>
2021-07-09 12:14:50 +0200

    Umożliwiono wywołanie skryptu z dowolnego miejsca.

--- smoothing/invokeTests.py
+++ smoothing/invokeTests.py
@@ -1,7 +1,13 @@
+import sys, os
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parents[0]))
+
 from framework.test.test_DefaultClasses import *
 from framework.test.test_smoothingFramework import *
-from framework.test.test_experiments import *
+#from framework.test.test_experiments import *
 import unittest
+
 
 if __name__ == "__main__":
     #sf.useDeterministic()
