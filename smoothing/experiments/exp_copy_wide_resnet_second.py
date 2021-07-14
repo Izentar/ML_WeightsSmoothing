8e5d50f7f5ae973dbf51494de928cfa04dff19b8
Mateusz Kordowski <mateuszkd67@gmail.com>
2021-07-15 00:47:30 +0200

    Nowy model  wide resnet

---
+++ smoothing/experiments/exp_copy_wide_resnet_second.py
@@ -0,0 +1,264 @@
+import experiments as experiments
+
+import torch
+import torchvision
+import torch.optim as optim
+import torch.optim.lr_scheduler as scheduler
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from framework import smoothingFramework as sf
+from framework import defaultClasses as dc
+
+import numpy as np
+import math
+
+"""
+Wide ResNet by Sergey Zagoruyko and Nikos Komodakis
+Fixup initialization by Hongyi Zhang, Yann N. Dauphin, Tengyu Ma
+Based on code by xternalz and Andy Brock: 
+https://github.com/xternalz/WideResNet-pytorch
+https://github.com/ajbrock/BoilerPlate
+"""
+
+
+class BasicBlock(nn.Module):
+    droprate = 0.0
+    use_bn = True
+    use_fixup = False
+    fixup_l = 12
+
+    def __init__(self, in_planes, out_planes, stride):
+        super(BasicBlock, self).__init__()
+
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.equalInOut = in_planes == out_planes
+        self.conv_res = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, padding=0, bias=False)
+        self.conv_res = not self.equalInOut and self.conv_res or None
+
+        assert self.use_fixup or self.use_bn, "Need to use at least one thing: Fixup or BatchNorm"
+
+        if self.use_fixup:
+            self.multiplicator = nn.Parameter(torch.ones(1,1,1,1))
+            self.biases = nn.ParameterList([nn.Parameter(torch.zeros(1,1,1,1))] * 4)
+
+            k = self.conv1.kernel_size[0] * self.conv1.kernel_size[1] * self.conv1.out_channels
+            self.conv1.weight.data.normal_(0, self.fixup_l ** (-0.5) * math.sqrt(2. / k)) 
+            self.conv2.weight.data.zero_()
+            
+            if self.conv_res is not None:
+                k = self.conv_res.kernel_size[0] * self.conv_res.kernel_size[1] * self.conv_res.out_channels
+                self.conv_res.weight.data.normal_(0, math.sqrt(2. / k))
+
+    def forward(self, x):
+        if self.use_bn:
+            x_out = self.relu(self.bn1(x))
+            out = self.relu(self.bn2(self.conv1(x_out)))
+            if self.droprate > 0:
+                out = F.dropout(out, p=self.droprate, training=self.training)
+            out = self.conv2(out)
+        else:
+            x_out = self.relu(x + self.biases[0])
+            out = self.conv1(x_out) + self.biases[1]
+            out = self.relu(out) + self.biases[2]
+            if self.droprate > 0:
+                out = F.dropout(out, p=self.droprate, training=self.training)
+            out = self.multiplicator * self.conv2(out) + self.biases[3]
+
+        if self.equalInOut:
+            return torch.add(x, out)
+
+        return torch.add(self.conv_res(x_out), out)
+
+
+class NetworkBlock(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, block, stride):
+        super(NetworkBlock, self).__init__()
+        self.layer = self._make_layer(block, in_planes, out_planes, nb_layers, stride)
+
+    def _make_layer(self, block, in_planes, out_planes, nb_layers, stride):
+        layers = []
+
+        for i in range(int(nb_layers)):
+            _in_planes = i == 0 and in_planes or out_planes
+            _stride = i == 0 and stride or 1
+            layers.append(block(_in_planes, out_planes, _stride))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layer(x)
+
+#@registry.Model
+class WideResNet(nn.Module):
+    def __init__(self, depth, num_classes, widen_factor=1, droprate=0.0, use_bn=True, use_fixup=False):
+        super(WideResNet, self).__init__()
+
+        nChannels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
+
+        assert (depth - 4) % 6 == 0, "You need to change the number of layers"
+        n = (depth - 4) / 6
+
+        BasicBlock.droprate = droprate
+        BasicBlock.use_bn = use_bn
+        BasicBlock.fixup_l = n * 3
+        BasicBlock.use_fixup = use_fixup
+        block = BasicBlock
+        
+        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1)
+        self.block2 = NetworkBlock(n, nChannels[1], nChannels[2], block, 2)
+        self.block3 = NetworkBlock(n, nChannels[2], nChannels[3], block, 2)
+        
+        self.bn1 = nn.BatchNorm2d(nChannels[3])
+        self.relu = nn.ReLU(inplace=True)
+        self.fc = nn.Linear(nChannels[3], num_classes)
+        self.nChannels = nChannels[3]
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                k = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / k))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+                if use_fixup:
+                    m.weight.data.zero_()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        return self.fc(out)
+
+"""
+Cutout implementation by Machine Learning Research Group at the University of Guelph:
+https://github.com/uoguelph-mlrg/Cutout
+"""
+
+
+class Cutout(object):
+    """Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
+
+    def __call__(self, img):
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+        h = img.size(1)
+        w = img.size(2)
+
+        mask = np.ones((h, w), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
+
+# wzorowane na pracy https://paperswithcode.com/paper/wide-residual-networks
+# model wzorowany na https://www.kaggle.com/itslek/cifar-10-96-pytorch-wresnet-28x10-sf-v3-2
+
+if(__name__ == '__main__'):
+    modelDevice = 'cuda:0'
+    if(sf.test_mode().isActive()):
+        modelDevice="cuda:0"
+
+
+    otherData = {
+        "IMG_MEAN":[125.3, 123.0, 113.9],
+        "IMG_STD":[63.0, 62.1, 66.7],
+        "loop":5,
+        "modelName":"wide_resnet",
+        "prefix":"set_copyOfExper_",
+        "runningAvgSize":10,
+        "num_classes":10,
+        "schedulerEpoches":[60, 90, 120, 150, 180],
+        "lr_sched_gamma":0.2
+    }
+
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in otherData["IMG_MEAN"]],
+                                     std=[x / 255.0 for x in otherData["IMG_STD"]])
+
+    metadata = sf.Metadata(testFlag=True, trainFlag=True, debugInfo=True)
+    dataMetadata = dc.DefaultData_Metadata(pin_memoryTest=False, pin_memoryTrain=False, epoch=170,
+        batchTrainSize=128, batchTestSize=100, startTestAtEpoch=[0, 50, 100, 150, 170], 
+        transformTrain=transforms.Compose([
+            #transforms.Lambda(lambda x: F.pad(x.unsqueeze(0), (4,4,4,4), mode="reflect").squeeze()),
+            #transforms.ToPILImage(),
+            transforms.RandomCrop(32),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+            Cutout(n_holes=1, length=5)
+        ]),
+        transformTest=transforms.Compose([
+            transforms.ToTensor(),
+            normalize
+        ]))
+    optimizerDataDict={"learning_rate":0.1, "momentum":0.9, "weight_decay":0.0005, "nesterov":True}
+    modelMetadata = dc.DefaultModel_Metadata(device=modelDevice, lossFuncDataDict={}, optimizerDataDict=optimizerDataDict)
+
+
+    types = ('predefModel', 'CIFAR10', 'disabled')
+    try:
+        stats = []
+        rootFolder = otherData["prefix"] + sf.Output.getTimeStr() + ''.join(x + "_" for x in types)
+        smoothingMetadata = dc.DisabledSmoothing_Metadata()
+
+        for r in range(otherData["loop"]):
+            obj = WideResNet(depth=28, widen_factor=10, droprate=0.3, num_classes=otherData["num_classes"], use_fixup=True,
+                use_bn=True)
+
+            data = dc.DefaultDataCIFAR10(dataMetadata)
+            model = dc.DefaultModelPredef(obj=obj, modelMetadata=modelMetadata, name=otherData["modelName"])
+            smoothing = dc.DisabledSmoothing(smoothingMetadata)
+
+            optimizer = optim.SGD(model.getNNModelModule().parameters(), lr=optimizerDataDict['learning_rate'], 
+                weight_decay=optimizerDataDict['weight_decay'], momentum=optimizerDataDict['momentum'], nesterov=optimizerDataDict['nesterov'])
+            scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=otherData["lr_sched_gamma"])
+            loss_fn = nn.CrossEntropyLoss()     
+
+            stat=dc.run(metadataObj=metadata, data=data, model=model, smoothing=smoothing, optimizer=optimizer, lossFunc=loss_fn,
+                modelMetadata=modelMetadata, dataMetadata=dataMetadata, smoothingMetadata=smoothingMetadata, rootFolder=rootFolder,
+                schedulers=[(otherData["schedulerEpoches"], scheduler)], logData=otherData)
+
+            stat.saveSelf(name="stat")
+
+            stats.append(stat)
+        experiments.printAvgStats(stats, metadata, runningAvgSize=otherData["runningAvgSize"])
+    except Exception as ex:
+        experiments.printException(ex, types)
